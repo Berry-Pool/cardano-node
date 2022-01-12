17ff2c7b0b5b877b11358315c19fa0943f62de66
Moritz Angermann <moritz.angermann@gmail.com>
2022-01-12 07:03:07 +0000

    Add cardano-node-capi
    This adds a basic foreign export library for the node. Providing
    the `void runNode(int argc, char ** argv)` as the c entry point.
    
    Arguments are parsed the same as the command line cardano-node.

---
+++ cardano-node-capi/src/Node.hs
@@ -0,0 +1,35 @@
+module Node where
+
+import           Data.Aeson (eitherDecodeStrict)
+import           Cardano.Node.Run (runNode)
+
+import           Foreign.Ptr (Ptr)
+import           Foreign.C (CString, peekCString)
+import           Foreign.Marshal.Array (peekArray)
+import           Data.ByteString.Char8 (pack)
+
+import           System.IO (hSetBuffering, BufferMode (LineBuffering), stdout)
+
+import           Options.Applicative
+import           Cardano.Node.Parsers (nodeCLIParser, parserHelpHeader, parserHelpOptions,
+                   renderHelpDoc)
+
+-- allow setting stdout to linebuffering if needed
+foreign export ccall setLineBuffering :: IO ()
+setLineBuffering :: IO ()
+setLineBuffering = hSetBuffering stdout LineBuffering
+
+-- | @crunNode@ is an exported C entry point to start a node.
+-- We parese the same arguments as the node CLI, but allow to
+-- pass the argumnets as @char *argv[]@ from C.
+foreign export ccall "runNode" crunNode :: Int -> Ptr CString -> IO ()
+crunNode :: Int -> Ptr CString -> IO ()
+crunNode argc argv = peekArray argc argv >>= mapM peekCString >>= \args ->
+    case execParserPure pref opts args of
+        Success pnc -> runNode pnc
+        Failure f   -> print f
+        CompletionInvoked _ -> putStrLn "Completion Invoked?"
+  where
+    pref = prefs showHelpOnEmpty
+    opts = info nodeCLIParser
+        ( fullDesc <> progDesc "Start node of the Cardano blockchain." )
